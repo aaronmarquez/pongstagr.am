6b102b799c8bd98a8f7812ce4528e1c637918623
Pongstr <twiz.tickler@gmail.com>
2013-06-24 01:07:16 +0800

    updates
    * completed documentation body.
    * re-added fonts glyphs and glyphicons.less

---
+++ docs/assets/js/docs.js
@@ -0,0 +1,84 @@
+// Pongstagr.am docs.js
+// Script requirements:
+//    + jQuery 1.8+
+//    + bootstrap.js
+//    + pongstagram.js 
+// ====================
+
+$(window).load(function(){
+  
+  "use strict";
+  
+  // Sticky Navbar
+  // =============
+  $('.sticky').affix();
+
+  // Smooth scroll for internal links
+  // ================================
+  $('.nav a[href^="#"]').on('click',function (e) {
+    e.preventDefault();
+    var target = this.hash,
+       $target = $(target);
+       
+    $('html, body').stop().animate({
+        'scrollTop': $target.offset().top
+    }, { duration: 1250, easing: 'easeInOutExpo'}, function () {
+        window.location.hash = target;
+        return false;
+    });
+
+    // Sub-Pages Navigation
+    // ====================
+    if ( $('.nav li').length > 0 ){
+      $('.nav li').removeClass('active');
+      $(this).addClass('active');
+    } 
+  });
+
+  // Update Navdots status onScroll
+  // ==============================
+  // $(window).scroll(function(){
+  //   var windowScroll = $(window).scrollTop();
+  //
+  //   if ( windowScroll >= 50 ){
+  //     $('[role="main"] > div').each(function(i){
+  //       if ($(this).position().top <= windowScroll + 50 ){
+  //         $('.nav a').removeClass('active');
+  //         $('.nav a').eq(i).addClass('active');
+  //       }
+  //     });
+  //   }
+  // }).scroll();
+    
+  
+  var usr = '39666111',
+      tkn = '39666111.1fb234f.c3901000b4944a549fd5fd2310c63780';
+
+    $('#recent').pongstgrm({
+      accessId    : usr,
+      accessToken : tkn,
+      show        : 'recent',
+      count       : 4,
+      pager       : true
+    });
+      
+    $('#liked').pongstgrm({
+      accessId    : usr,
+      accessToken : tkn,
+      show        : 'liked',
+      count       : 4,
+      pager       : true
+    });  
+  
+      
+    $('#feed').pongstgrm({
+      accessId    : usr,
+      accessToken : tkn,
+      show        : 'feed',
+      count       : 4,
+      pager       : true
+    });  
+  
+  
+  
+});
